244fa2e04ef322fe11d3e67a7a854a49df3d4244
HARSHA-VSK1 <harshavsk2002@gmail.com>
2022-03-19 10:22:18 +0530

    First commit

---
+++ face_recog.py
@@ -0,0 +1,105 @@
+import sys
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import pandas as pd
+
+# from PIL import ImageGrab
+
+
+path = 'images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+
+        myDataList = f.readlines()
+        #print(myDataList)
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            #print(entry)
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
+
+#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+# def captureScreen(bbox=(300,300,690+300,530+300)):
+#     capScr = np.array(ImageGrab.grab(bbox))
+#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#     return capScr
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+print(len(encodeListKnown))
+
+cap = cv2.VideoCapture(0)
+names=[]
+
+
+while True:
+    success, img = cap.read()
+# img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+#finding the matches
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        #print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if faceDis[matchIndex]< 0.50:
+          name = classNames[matchIndex].upper()
+          if names.count(name)<=2:
+            names.append(name)
+
+          if names.count(name)==1:
+            markAttendance(name)
+        else: name = 'Unknown'
+#print(name)
+        y1,x2,y2,x1 = faceLoc
+        #for up-scaling the image
+        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(5)
+    df = pd.read_csv('Attendance.csv')
+
+    new_df = df[['Name', 'Time']].drop_duplicates()
+
+    new_df.to_csv('out.csv', index=False)
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+
+print("open out.csv to view the attendance report")
